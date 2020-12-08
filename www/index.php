c7c96537f8d73f9b3b96b95c0b046730714164ea
KirylMi <xxxdendelxxx@gmail.com>
2020-12-08 23:30:46 +0300

    Drinks photos on web site.. Styles tbd

--- www/index.php
+++ www/index.php
@@ -1,9 +1,15 @@
 <?php
 $dbh = new PDO('pgsql:dbname=myDb;host=172.20.0.2', 'postgres', 'test');
-$stmt = $dbh->prepare("SELECT * FROM public.users;");
+$stmt = $dbh->prepare("SELECT * FROM \"oxotaK\".drinks;");
 $stmt->execute();
 while ($result=$stmt->fetch(PDO::FETCH_ASSOC)){
-    var_dump($result); echo "</br>";
+    ob_start();
+    fpassthru($result['photo']);
+    $dat= ob_get_contents();
+    ob_end_clean();
+    $dat= "data:image/*;base64," . base64_encode($dat);
+    echo "<img src='".$dat."'";
+    var_dump($result['photo']); echo "</br>";
 }
 //var_dump ($stmt->fetch(PDO::FETCH_ASSOC));
 echo "Welcome, welcome";
